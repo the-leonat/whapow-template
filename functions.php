918abaeda54f2c65156cf0152e7ab5fbfe118e99
Leo <130.0x82@gmail.com>
2020-02-21 13:48:43 +0100

    remove shopping cart from startpage

--- functions.php
+++ functions.php
@@ -78,6 +78,14 @@ function get_variable_product_by_sku(string $sku)
     return new WC_Product_Variable($id);
 }
 
+
+// remove cart
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+
+
 // REMOVE BREADCUMB
 
 add_action('init', 'storefront_loaded');
